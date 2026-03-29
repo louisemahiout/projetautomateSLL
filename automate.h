7a48b6bd5fa2fb0287980f4877f7775683e66eda
Sarra-Ghedira2005 <sarra.ghedira@efrei.net>
2026-03-29 20:36:48 +0200

    commentaire

--- automate.h
+++ automate.h
@@ -9,22 +9,29 @@
 #define MAX_SYMBOLES 26
 #define MAX_NOM       60
 
+// Structure représentant un automate fini
 typedef struct {
-    int  nb_symboles;
-    char symboles[MAX_SYMBOLES][5];
+    int  nb_symboles;                         // Nombre de symboles dans l'alphabet
+    char symboles[MAX_SYMBOLES][5];           // Liste des symboles (chaînes de 1 à 4 caractères + 'ε')
 
-    int  nb_etats;
-    char etats[MAX_ETATS][30];      /* 30 au lieu de 10 : noms composés type "0.1.3.5" */
+    int  nb_etats;                            // Nombre total d'états dans l'automate
+    char etats[MAX_ETATS][30];                /* Noms des états : jusqu'à 30 caractères pour gérer des
+                                                 états composés du type "0.1.3.5" */
 
-    int  nb_initiaux;
-    char initiaux[MAX_ETATS][30];
+    int  nb_initiaux;                         // Nombre d'états initiaux
+    char initiaux[MAX_ETATS][30];             // Noms des états initiaux
 
-    int  nb_finaux;
-    char finaux[MAX_ETATS][30];
+    int  nb_finaux;                           // Nombre d'états finaux
+    char finaux[MAX_ETATS][30];               // Noms des états finaux
 
     char transitions[MAX_ETATS][MAX_SYMBOLES][MAX_ETATS][30];
-    int  nb_transitions[MAX_ETATS][MAX_SYMBOLES];
+    /* Table des transitions :
+       transitions[i][j][k] = nom de l'état k atteint
+       depuis l'état i avec le symbole j */
 
+    int  nb_transitions[MAX_ETATS][MAX_SYMBOLES];
+    /* Nombre de transitions existantes depuis l'état i
+       avec le symbole j */
 } Automate;
 
 /* fonctions */
